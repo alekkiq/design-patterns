646c2fe084c261de5bb54aa85b711684a832111c
Aleksi Putkonen <aleksi.putkonen1@gmail.com>
2026-03-10 17:24:10 +0200

    Add Message class and MessageType enum for message types

---
+++ src/ChainOfResponsibility/Message.java
@@ -0,0 +1,37 @@
+package ChainOfResponsibility;
+
+public class Message {
+    private MessageType type;
+    private String content;
+    private String senderEmail;
+
+    public Message(MessageType type, String content, String senderEmail) {
+        this.type = type;
+        this.content = content;
+        this.senderEmail = senderEmail;
+    }
+
+    public MessageType getType() {
+        return this.type;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public String getSenderEmail() {
+        return this.senderEmail;
+    }
+
+    public void setType(MessageType type) {
+        this.type = type;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setSenderEmail(String senderEmail) {
+        this.senderEmail = senderEmail;
+    }
+}
---
+++ src/ChainOfResponsibility/MessageType.java
@@ -0,0 +1,8 @@
+package ChainOfResponsibility;
+
+public enum MessageType {
+    COMPENSATION_CLAIM,
+    CONTACT_REQUEST,
+    SUGGESTION,
+    FEEDBACK
+}
