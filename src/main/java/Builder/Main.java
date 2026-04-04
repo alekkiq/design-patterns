7f6fbdc0fe895fc3e660f8bffeae5e36fe5a5712
Aleksi Putkonen <aleksi.putkonen1@gmail.com>
2026-04-04 13:32:29 +0300

    Add Main to demonstrate computer builder

---
+++ src/main/java/Builder/Main.java
@@ -0,0 +1,13 @@
+package Builder;
+
+public class Main {
+    public static void main(String[] args) {
+        ComputerBuilder builder = new OfficeComputerBuilder();
+        ComputerDirector director = new ComputerDirector(builder);
+
+        director.constructComputer();
+        Computer computer = builder.getComputer();
+
+        System.out.println(computer);
+    }
+}
